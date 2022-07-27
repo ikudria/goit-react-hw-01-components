090632fba3ab015afbd6b533dc964a3c4bb0bc7b
ikudria <irakudrya@gmail.com>
2022-07-26 20:01:07 -0500

    Created basic markup for components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+
+
+
+
 export const App = () => {
   return (
     <div
       style={{
+        
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 20,
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile />
+      <Statistics />
+      <FriendList />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,9 @@
+import FriendListItem from '../FriendListItem/FriendListItem';
+
+const FriendList = () => {
+    return (<ul class="friend-list">
+        <FriendListItem />
+    </ul>)
+};
+export default FriendList;
+
